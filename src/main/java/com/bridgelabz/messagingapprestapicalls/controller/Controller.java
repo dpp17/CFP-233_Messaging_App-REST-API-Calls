2e9edf9156821bd4eb1e7ef40f75a8b7a454e9cd
dpp17 <durga123q.com>
2023-04-11 07:30:02 +0530

    Added:: REST Call to show Hello

--- src/main/java/com/bridgelabz/messagingapprestapicalls/controller/Controller.java
+++ src/main/java/com/bridgelabz/messagingapprestapicalls/controller/Controller.java
@@ -12,4 +12,11 @@ public class Controller {
         return "Hello from BridgeLabz";
     }
 
+    //////////////////// ::  UC-2  :: ////////////////////
+    @RequestMapping(value = "/queryparam", method = RequestMethod.GET)     //handle the all the Http request From User---need to define path and define method as get,post,put or delete
+    public String message(@RequestParam(value = "name", defaultValue = "Bridgelabz") String name){
+        return " Hello  "+ name;                                     //-->> localhost:8080/queryparam?message=<pass a String>
+    }
+
+
 }
